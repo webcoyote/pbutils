81747ee26b90ebab01ddafdddf2d485d8d456439
Patrick Wyatt <pat@codeofhonor.com>
2019-09-20 10:47:46 -0700

    First commit

---
+++ pbcopy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PbCopy
+{
+    class Program {
+        [STAThread]
+        static void Main(string[] args) {
+            string s;
+            StringBuilder output = new StringBuilder( string.Join(" ", args) );
+            while ((s = Console.ReadLine()) != null)
+                output.AppendLine(s);
+
+            Clipboard.SetText(output.ToString());
+        }
+    }
+}
+
---
+++ pbpaste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PbPaste {
+    class Program {
+        [STAThread]
+        static void Main(string[] args) {
+            Console.Write(Clipboard.GetText());
+        }
+    }
+}
