c0857ca0cc91b08d6896536a6ebf23509daf50f2
Patrick Wyatt <pat@codeofhonor.com>
2019-11-24 13:42:42 -0800

    Add bringtofront app

---
+++ bringtofront/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices; 
+
+namespace BringToFront {
+    class Program {
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(String lpClassName, String lpWindowName);
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("User32.dll")]
+        private static extern bool IsIconic(IntPtr handle);
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindow(IntPtr handle, int nCmdShow);
+        const int SW_RESTORE = 9;
+
+        public static void BringToFront(string windowClass) {
+            IntPtr handle = FindWindow(windowClass, null);
+            if (handle == IntPtr.Zero)
+                return;
+
+            if (IsIconic(handle))
+                ShowWindow(handle, SW_RESTORE);
+            SetForegroundWindow(handle);
+        }
+
+        static void Usage () {
+            Console.WriteLine(
+                "USAGE: BringToFront WINDOWCLASS"
+            );
+        }
+
+        static void Main(string[] args) {
+            if (args.Length != 1) {
+                Usage();
+                return;
+            }
+
+            BringToFront(args[0]);
+        }
+    }
+}
